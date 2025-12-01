f77f53d415cdb56598f218fd44514ee54a61be41
Wasiul Islam Sakif <sakif049@gmail.com>
2025-12-01 12:43:41 +0600

    fix: player UI consistancy

--- lib/views/base/video_widget.dart
+++ lib/views/base/video_widget.dart
@@ -38,6 +38,14 @@ class _VideoWidgetState extends State<VideoWidget> {
           ..setLooping(true)
           ..setVolume(1);
     flickManager = FlickManager(videoPlayerController: _controller);
+    _controller.addListener(() {
+      if (_controller.value.hasError) {
+        setState(() {});
+        debugPrint("VIDEO ERROR: ${_controller.value.errorDescription}");
+      }
+    });
+
+    debugPrint(widget.url.toString());
   }
 
   @override
@@ -52,168 +60,173 @@ class _VideoWidgetState extends State<VideoWidget> {
       flickManager: flickManager,
       flickVideoWithControls: FlickVideoWithControls(
         videoFit: BoxFit.cover,
-        playerLoadingFallback: Stack(
-          children: [
-            Center(
-              child: CircularProgressIndicator(
-                color: AppColors.white,
-                constraints: BoxConstraints(minHeight: 30, minWidth: 30),
-              ),
-            ),
-            Positioned(
-              top: 12,
-              left: 20,
-              child: FlickAutoHideChild(
-                child: SafeArea(
-                  child: GestureDetector(
-                    onTap: () {
-                      Get.back();
-                    },
-                    child: Container(
-                      height: 48,
-                      width: 48,
-                      decoration: BoxDecoration(
-                        color: Color(0xff1b1b1b).withAlpha(128),
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        child: CustomSvg(asset: "assets/icons/back.svg", size: 32,),
-                      ),
+        playerLoadingFallback: playerOverlay(isLoading: true),
+        playerErrorFallback: playerOverlay(isError: true),
+        controls: CustomFlickPortraitControls(postData: widget.postData),
+      ),
+    );
+  }
+
+  Stack playerOverlay({bool isLoading = false, bool isError = false}) {
+    return Stack(
+      children: [
+        if (isError)
+          Positioned.fill(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  spacing: 4,
+                  children: [
+                    Icon(Icons.error_outline),
+                    Text(
+                      "Error Occured!",
+                      style: AppTexts.tmdm.copyWith(color: AppColors.gray[25]),
                     ),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    _controller.value.errorDescription ?? "",
+                    textAlign: TextAlign.center,
                   ),
+                ),
+              ],
+            ),
+          ),
+        if (isLoading)
+          Center(
+            child: CircularProgressIndicator(
+              color: AppColors.white,
+              constraints: BoxConstraints(minHeight: 30, minWidth: 30),
+            ),
+          ),
+        Positioned(
+          top: 12,
+          left: 20,
+          child: SafeArea(
+            child: GestureDetector(
+              onTap: () {
+                Get.back();
+              },
+              child: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.white.withValues(alpha: 0.30),
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: CustomSvg(asset: "assets/icons/back.svg", size: 32),
                 ),
               ),
             ),
-            Positioned.fill(
-              child: FlickAutoHideChild(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      // color: Color(0xff1b1b1b).withAlpha(128),
-                      gradient: LinearGradient(
-                        begin: AlignmentGeometry.bottomCenter,
-                        end: AlignmentGeometry.topCenter,
-                        colors: [Colors.black, Colors.transparent],
+          ),
+        ),
+        Positioned.fill(
+          child: FlickAutoHideChild(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  // color: Color(0xff1b1b1b).withAlpha(128),
+                  gradient: LinearGradient(
+                    begin: AlignmentGeometry.bottomCenter,
+                    end: AlignmentGeometry.topCenter,
+                    colors: [Colors.black, Colors.transparent],
+                  ),
+                ),
+                padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                child: SafeArea(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // Text(
+                      //   "Post from event",
+                      //   style: AppTexts.tmdr.copyWith(
+                      //     color: AppColors.gray[25],
+                      //   ),
+                      // ),
+                      // const SizedBox(height: 12),
+                      Text(
+                        widget.postData.title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: AppTexts.dxsm.copyWith(
+                          color: AppColors.gray[25],
+                        ),
                       ),
-                    ),
-                    padding: EdgeInsets.only(left: 20, right: 20, top: 20),
-                    child: SafeArea(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          // Text(
-                          //   "Post from event",
-                          //   style: AppTexts.tmdr.copyWith(
-                          //     color: AppColors.gray[25],
-                          //   ),
-                          // ),
-                          // const SizedBox(height: 12),
+                      const SizedBox(height: 4),
+                      Row(
+                        children: [
                           Text(
-                            widget.postData.title,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: AppTexts.dxsm.copyWith(
+                            Get.find<PostController>().getDistance(
+                              widget.postData.location.coordinates[0],
+                              widget.postData.location.coordinates[1],
+                            ),
+                            style: AppTexts.tmdm.copyWith(
                               color: AppColors.gray[25],
                             ),
                           ),
-                          const SizedBox(height: 4),
-                          Row(
-                            children: [
-                              Text(
-                                Get.find<PostController>().getDistance(
-                                  widget.postData.location.coordinates[0],
-                                  widget.postData.location.coordinates[1],
-                                ),
-                                style: AppTexts.tmdm.copyWith(
-                                  color: AppColors.gray[25],
-                                ),
-                              ),
-                              const SizedBox(width: 8),
-                              for (int i = 0; i < 4; i++)
-                                CustomSvg(asset: "assets/icons/star.svg"),
-                              for (int i = 0; i < 1; i++)
-                                CustomSvg(
-                                  asset: "assets/icons/star.svg",
-                                  color: Colors.white,
-                                ),
-                              const SizedBox(width: 4),
-                              Text(
-                                widget.postData.averageRating.toString(),
-                                style: AppTexts.tmdm.copyWith(
-                                  color: AppColors.gray[25],
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 12),
-                          Row(
-                            children: [
-                              ProfilePicture(
-                                image: widget.postData.author.image,
-                                size: 52,
-                              ),
-                              const SizedBox(width: 8),
-                              Text(
-                                widget.postData.author.name,
-                                style: AppTexts.txlm.copyWith(
-                                  color: AppColors.gray[25],
-                                ),
-                              ),
-                            ],
-                          ),
-                          FlickVideoProgressBar(
-                            flickProgressBarSettings: FlickProgressBarSettings(
-                              playedColor: AppColors.green,
+                          const SizedBox(width: 8),
+                          for (int i = 0; i < 4; i++)
+                            CustomSvg(asset: "assets/icons/star.svg"),
+                          for (int i = 0; i < 1; i++)
+                            CustomSvg(
+                              asset: "assets/icons/star.svg",
+                              color: Colors.white,
                             ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              FlickCurrentPosition(fontSize: 14),
-                              Spacer(),
-                              FlickTotalDuration(fontSize: 14),
-                            ],
+                          const SizedBox(width: 4),
+                          Text(
+                            widget.postData.averageRating.toString(),
+                            style: AppTexts.tmdm.copyWith(
+                              color: AppColors.gray[25],
+                            ),
                           ),
                         ],
                       ),
-                    ),
+                      const SizedBox(height: 12),
+                      Row(
+                        children: [
+                          ProfilePicture(
+                            image: widget.postData.author.image,
+                            size: 52,
+                          ),
+                          const SizedBox(width: 8),
+                          Text(
+                            widget.postData.author.name,
+                            style: AppTexts.txlm.copyWith(
+                              color: AppColors.gray[25],
+                            ),
+                          ),
+                        ],
+                      ),
+                      FlickVideoProgressBar(
+                        flickProgressBarSettings: FlickProgressBarSettings(
+                          playedColor: AppColors.green,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          FlickCurrentPosition(fontSize: 14),
+                          Spacer(),
+                          FlickTotalDuration(fontSize: 14),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ),
             ),
-          ],
+          ),
         ),
-        playerErrorFallback: Stack(
-          children: [
-            Center(child: Icon(Icons.error_outline)),
-            Positioned(
-              top: 12,
-              left: 20,
-              child: FlickAutoHideChild(
-                child: GestureDetector(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: Container(
-                    height: 32,
-                    width: 32,
-                    decoration: BoxDecoration(
-                      color: Color(0xff1b1b1b).withAlpha(128),
-                      shape: BoxShape.circle,
-                    ),
-                    child: Center(
-                      child: CustomSvg(asset: "assets/icons/back.svg"),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-        controls: CustomFlickPortraitControls(postData: widget.postData),
-      ),
+      ],
     );
   }
 }
